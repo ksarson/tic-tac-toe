a83da2e968d857f9dd1446e0ef128b9d15d496e2
ksarson <37591778+ksarson@users.noreply.github.com>
2023-11-05 16:30:03 -0500

    Completed tic-tac-toe game

--- ticTacToe.js
+++ ticTacToe.js
@@ -5,14 +5,25 @@ const gameBoard = {
         ['', '', ''],
     ],
 
-    initializeGame(player1Info, player2Info, winnerInfo) {
-        player1Info.textContent = `Player 1: ${player1.score}`;
-        player2Info.textContent = `Player 2: ${player2.score}`;
-        winnerInfo.textContent = '';
+    initializeGame(player1Info, player2Info, winnerInfo, newGameButton) {
+        player1Info.textContent = `P1 - ${player1.score}`;
+        player2Info.textContent = `P2 - ${player2.score}`;
+        winnerInfo.textContent = `${playerTurn.name} (${playerTurn.symbol}'s) turn`;
+        newGameButton.style.display = 'none';
+        gameBoard.board = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', ''],
+        ];
     },
 
-    updateBoard(tile) {
-        tile.innerText = playerTurn.symbol;
+    updateBoard(tile, symbol, playerTurn) {
+        tile.innerText = symbol;
+        if (playerTurn === player1) {
+            tile.style.color = 'var(--green)';
+        } else if (playerTurn === player2) {
+            tile.style.color = 'var(--purple-light)';
+        }
     },
 
     isBoardFull() {
@@ -65,6 +76,18 @@ const gameBoard = {
 
         return false;
     },
+
+    handleScoreColors(player1Score, player2Score, player1Info, player2Info) {
+        if (player1Score === player2Score) {
+            player1Info.style.color = 'white';
+            player2Info.style.color = 'white';
+        } else {
+            player1Info.style.color =
+                player1Score > player2Score ? 'green' : 'red';
+            player2Info.style.color =
+                player2Score > player1Score ? 'green' : 'red';
+        }
+    },
 };
 
 function createPlayer(name, symbol) {
@@ -78,22 +101,53 @@ function createPlayer(name, symbol) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    // #region initialize game
     const player1Info = document.getElementById('player1');
     const player2Info = document.getElementById('player2');
-    const winnerInfo = document.getElementById('current-winner');
-    gameBoard.initializeGame(player1Info, player2Info, winnerInfo);
-
+    const winnerInfo = document.getElementById('winner-text');
+    const newGameButton = document.getElementById('new-game-button');
     const buttons = document.querySelectorAll('.tile-button');
+
+    gameBoard.initializeGame(
+        player1Info,
+        player2Info,
+        winnerInfo,
+        newGameButton
+    );
+    // #endregion
+
+    // #region new game buttton
+    newGameButton.addEventListener('click', function () {
+        console.log('clicked');
+        gameBoard.initializeGame(
+            player1Info,
+            player2Info,
+            winnerInfo,
+            newGameButton
+        );
+
+        buttons.forEach((button) => {
+            gameBoard.updateBoard(button, '', null);
+            button.disabled = false;
+        });
+    });
+    // #endregion
+
     buttons.forEach((button) => {
         button.addEventListener('click', function () {
+            // #region playing turns
             const [row, col] = button.parentElement.id.split('-').map(Number);
             if (gameBoard.board[row][col].length === 0) {
                 gameBoard.board[row][col] = playerTurn.symbol;
-                gameBoard.updateBoard(button);
+                gameBoard.updateBoard(button, playerTurn.symbol, playerTurn);
                 playerTurn = playerTurn === player1 ? player2 : player1;
+                winnerInfo.textContent = `${playerTurn.name} (${playerTurn.symbol}'s) turn`;
             } else {
                 alert('Please make a valid selection!');
             }
+            // #endregion
+
+            // #region game won
             if (gameBoard.isGameWon()) {
                 const winner = gameBoard.isGameWon();
                 let winningPlayer;
@@ -104,19 +158,44 @@ document.addEventListener('DOMContentLoaded', function () {
                     player2.score++;
                     winningPlayer = player2.name;
                 }
-                player1Info.textContent = `Player 1: ${player1.score}`;
-                player2Info.textContent = `Player 2: ${player2.score}`;
+                player1Info.textContent = `P1 - ${player1.score}`;
+                player2Info.textContent = `P2 - ${player2.score}`;
                 winnerInfo.textContent = `Winner is ${winningPlayer}!`;
+                buttons.forEach((button) => {
+                    button.disabled = true;
+                });
+                newGameButton.style.display = 'block';
+                gameBoard.handleScoreColors(
+                    player1.score,
+                    player2.score,
+                    player1Info,
+                    player2Info
+                );
                 return;
             }
+            // #endregion
+
+            // #region game draw
             if (gameBoard.isBoardFull()) {
                 winnerInfo.textContent = `Draw!`;
+                buttons.forEach((button) => {
+                    button.disabled = true;
+                });
+                newGameButton.style.display = 'block';
+                gameBoard.handleScoreColors(
+                    player1.score,
+                    player2.score,
+                    player1Info,
+                    player2Info
+                );
+
                 return;
             }
+            // #endregion
         });
     });
 });
 
-const player1 = createPlayer('Player 1', 'X');
-const player2 = createPlayer('Player 2', 'O');
+const player1 = createPlayer('Player 1', '❤');
+const player2 = createPlayer('Player 2', '♥');
 let playerTurn = player1;
